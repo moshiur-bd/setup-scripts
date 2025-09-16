b0142d6c5921cf0941035db0046e72a8d8e1da66
Moshiur Rahman <moshiur.rabby@gmail.com>
2025-09-16 00:14:12 +0000

    Basic ubuntu 24 bootstrap

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installing necessary on a remote machine
+
+
+### Available scripts
+ -- bootstrap-linux24.sh : Takes a linux24 machine and install necessary software to work with it
---
+++ bootstrap-ubuntu24.sh
@@ -0,0 +1,23 @@
+echo "Update packages"
+sudo apt -y update
+sudo apt -y upgrade
+
+echo "Install fish, unzip, make, gcc, git, ..."
+sudo apt -y install fish unzip make gcc ripgrep git xclip
+
+echo "Install neovim"
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt -y update
+sudo apt -y install neovim
+
+echo "Setup neovim scripts"
+curl -LO https://github.com/moshiur-bd/nvim/archive/refs/heads/remote.zip
+unzip remote.zip && rm remote.zip
+sudo mv -f nvim-remote ~/.config/nvim
+
+
+echo "Install minikube"
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-arm64
+sudo install minikube-linux-arm64 /usr/local/bin/minikube && rm minikube-linux-arm64
+

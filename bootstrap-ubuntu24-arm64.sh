b60c61ed8ec88b7f164b16cc16b288da42c03a23
Moshiur Rahman <moshiur.rabby@gmail.com>
2025-09-18 00:37:29 +0100

    get k8s running

---
+++ bootstrap-ubuntu24-arm64.sh
@@ -0,0 +1,31 @@
+echo "Update packages"
+sudo apt -y update
+sudo apt -y upgrade
+
+echo "Install fish, unzip, make, gcc, git, ..."
+sudo apt -y install fish unzip make gcc ripgrep git xclip
+
+echo "Install neovim"
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt -y update
+sudo apt -y install neovim
+
+echo "Setup neovim scripts"
+curl -LO https://github.com/moshiur-bd/nvim/archive/refs/heads/remote.zip
+unzip remote.zip && rm remote.zip
+mkdir  ~/.config
+sudo mv -f nvim-remote ~/.config/nvim
+sudo rm -rf nvim-remote #in case the above command fails due to pre-existing config
+
+if ! grep -q 'fish' ~/.bashrc; then
+    echo "Make fish default"
+    echo "fish" >> ~/.bashrc
+fi
+
+echo "Install minikube"
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-arm64
+sudo install minikube-linux-arm64 /usr/local/bin/minikube && rm minikube-linux-arm64
+
+
+echo "Install kubectl"
+snap install kubectl --classic
--- bootstrap-ubuntu24-x64.sh
+++ bootstrap-ubuntu24-x64.sh
@@ -10,6 +10,9 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt -y update
 sudo apt -y install neovim
 
+echo "Install tmux"
+sudo apt -y install tmux
+
 echo "Setup neovim scripts"
 curl -LO https://github.com/moshiur-bd/nvim/archive/refs/heads/remote.zip
 unzip remote.zip && rm remote.zip
@@ -22,7 +25,16 @@ if ! grep -q 'fish' ~/.bashrc; then
     echo "fish" >> ~/.bashrc
 fi
 
-echo "Install minikube"
-curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-arm64
-sudo install minikube-linux-arm64 /usr/local/bin/minikube && rm minikube-linux-arm64
+echo "Setup Docker"
+sudo apt -y install docker.io
 
+echo "Install minikube X64"
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+
+echo "Install kubectl"
+snap install kubectl --classic
+
+echo "Start kubernetes"
+minikube start --force
